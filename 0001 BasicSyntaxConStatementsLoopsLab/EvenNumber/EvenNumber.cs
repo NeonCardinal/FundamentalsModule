e957848244a89f1920efd1e7dcea875f6c4b888f
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 21:19:13 +0300

    add

---
+++ 0001 BasicSyntaxConStatementsLoopsLab/EvenNumber/EvenNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class EvenNumber
+{
+    static void Main()
+    {
+        while(true)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+            }
+            else
+            {
+                Console.WriteLine($"The number is: {Math.Abs(number)}");
+                break;
+            }
+        }
+    }
+}
