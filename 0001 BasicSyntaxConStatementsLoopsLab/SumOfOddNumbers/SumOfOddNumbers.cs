9b54e940c64c9c4950ae5191c6cb6812e7c2245d
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 21:22:12 +0300

    add

---
+++ 0001 BasicSyntaxConStatementsLoopsLab/SumOfOddNumbers/SumOfOddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class SumOfOddNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int counter = 0;
+
+        for(int i = 1; i <= 100; i += 2)
+        {
+            Console.WriteLine(i);
+            sum += i;
+            counter++;
+
+            if(counter == n)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
