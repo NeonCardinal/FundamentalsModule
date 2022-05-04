abea01bad2ed15411b989a4ac41cba0008ab95c0
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-05 00:09:03 +0300

    add

---
+++ 0003 BasicSyntaxConStatLoopsMoreExercise/ReverseString/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ReverseString
+{
+    static void Main()
+    {
+        string str;
+
+        str = Console.ReadLine();
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            Console.Write(str[i]);
+        }
+
+        Console.WriteLine();
+    }
+}
