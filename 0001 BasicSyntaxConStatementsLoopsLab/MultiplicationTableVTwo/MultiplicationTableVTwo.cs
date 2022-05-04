822576ded150370cfaa378efdd18bb6910a3e5b6
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 21:20:59 +0300

    add

---
+++ 0001 BasicSyntaxConStatementsLoopsLab/MultiplicationTableVTwo/MultiplicationTableVTwo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class MultiplicationTableVTwo
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int multiplier = int.Parse(Console.ReadLine());
+
+        if (multiplier > 10)
+        {
+            Console.WriteLine($"{number} X {multiplier} = {number * multiplier}");
+        }
+        else
+        {
+            for(int i = multiplier; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+        }
+    }
+}
