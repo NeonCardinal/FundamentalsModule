4d3337b3696ceefe5edb64650b2f4a789380dbf1
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 23:47:35 +0300

    add

---
+++ 0002 BasicSyntaxConStatementsLoopsExercise/Login/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Login
+{
+    static void Main()
+    {
+        string username = Console.ReadLine();
+
+        int counter = 0;
+        string pass = string.Empty;
+
+        for(int i = username.Length - 1; i >= 0; i--)
+        {
+            pass += username[i];
+        }
+
+        while(true)
+        {
+            string password = Console.ReadLine();
+
+            if(pass == password)
+            {
+                Console.WriteLine($"User {username} logged in.");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password. Try again.");
+                counter++;
+            }
+
+            if(counter >= 3)
+            {
+                Console.WriteLine($"User {username} blocked!");
+                break;
+            }
+        }
+    }
+}
