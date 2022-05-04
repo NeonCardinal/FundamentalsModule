481c1f346fd126c5be05b3841bdaf23094f25e00
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 21:18:10 +0300

    add

---
+++ 0001 BasicSyntaxConStatementsLoopsLab/BackInMinutes/BackInMinutes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class BackInMinutes
+{
+    static void Main()
+    {
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        minutes += 30;
+
+        if(minutes > 59)
+        {
+            hours++;
+            minutes -= 60;
+        }
+        if (hours > 23)
+        {
+            hours -= 24;
+        }
+
+        if(minutes < 10)
+        {
+            Console.WriteLine($"{hours}:0{minutes}");
+        }
+        else
+        {
+            Console.WriteLine($"{hours}:{minutes}");
+        }
+    }
+}
