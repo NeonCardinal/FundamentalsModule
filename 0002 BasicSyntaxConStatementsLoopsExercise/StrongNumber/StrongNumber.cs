32609460c822b67025623eace206ab415559c63e
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 23:49:01 +0300

    add

---
+++ 0002 BasicSyntaxConStatementsLoopsExercise/StrongNumber/StrongNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class StrongNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        int tempNumber = n;
+
+        while(tempNumber > 0)
+        {
+            int currNumber = tempNumber % 10;
+            tempNumber /= 10;
+            int currFact = 1;
+
+            for(int i = 1; i <= currNumber; i++)
+            {
+                currFact *= i;
+            }
+
+            sum += currFact;
+        }
+
+        if(n == sum)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
