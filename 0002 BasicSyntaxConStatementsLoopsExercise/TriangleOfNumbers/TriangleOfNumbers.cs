4b7120c8497ecc61002e838f41537cca00429a6f
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-04 23:49:20 +0300

    add

---
+++ 0002 BasicSyntaxConStatementsLoopsExercise/TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class TriangleOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int number = 1;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < number; j++)
+            {
+                Console.Write($"{number} ");
+            }
+
+            number++;
+            Console.WriteLine();
+        }
+    }
+}
