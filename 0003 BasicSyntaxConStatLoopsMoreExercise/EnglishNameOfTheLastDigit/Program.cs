18b745c3b8b4e24b4ec7f8d1a430bfa79b3e01d4
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-05 00:08:13 +0300

    add

---
+++ 0003 BasicSyntaxConStatLoopsMoreExercise/EnglishNameOfTheLastDigit/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+public class EnglishNameOfTheLastDigit
+{
+    static void Main()
+    {
+        int number;
+
+        number = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(DigitToText(number));
+    }
+
+    static string DigitToText(int number)
+    {
+        int digit = number % 10;
+        string text = "";
+
+        switch (digit)
+        {
+            case 0:
+                {
+                    text += "zero";
+                }
+                break;
+            case 1:
+                {
+                    text += "one";
+                }
+                break;
+            case 2:
+                {
+                    text += "two";
+                }
+                break;
+            case 3:
+                {
+                    text += "three";
+                }
+                break;
+            case 4:
+                {
+                    text += "four";
+                }
+                break;
+            case 5:
+                {
+                    text += "five";
+                }
+                break;
+            case 6:
+                {
+                    text += "six";
+                }
+                break;
+            case 7:
+                {
+                    text += "seven";
+                }
+                break;
+            case 8:
+                {
+                    text += "eight";
+                }
+                break;
+            case 9:
+                {
+                    text += "nine";
+                }
+                break;
+            default:
+                break;
+        }
+
+        return text;
+    }
+}
