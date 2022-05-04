7c7295474d63c4d18b3c1c9b0c51b6225a033fe3
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-05 00:09:19 +0300

    add

---
+++ 0003 BasicSyntaxConStatLoopsMoreExercise/SortNumbers/SortNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class SortNumbers
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+        int[] array = new int[3];
+
+        array[0] = firstNumber;
+        array[1] = secondNumber;
+        array[2] = thirdNumber;
+
+        Array.Sort(array);
+        Array.Reverse(array);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+    }
+}
