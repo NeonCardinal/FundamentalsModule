ce6910c3d8a06a0fd8eaa9ec12d6c5018b6600b4
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-06 16:57:26 +0300

    add

---
+++ 0004 DataTypesAndVariablesLab/ConcatNames/ConcatNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ConcatNames
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        string delimeter = Console.ReadLine();
+
+        Console.WriteLine($"{firstName}{delimeter}{lastName}");
+    }
+}
