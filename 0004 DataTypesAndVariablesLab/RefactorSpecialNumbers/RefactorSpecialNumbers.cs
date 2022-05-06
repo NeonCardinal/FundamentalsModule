ef89166f356ce627ad57dec69d400e3f62b336e6
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-06 16:58:49 +0300

    add

---
+++ 0004 DataTypesAndVariablesLab/RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class RefactorSpecialNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        bool isSpecial = false;
+        for (int i = 1; i <= number; i++)
+        {
+            int sum = 0;
+            int digits = i;
+            while (digits > 0)
+            {
+                sum += digits % 10;
+                digits = digits / 10;
+            }
+            isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+            Console.WriteLine("{0} -> {1}", i, isSpecial);
+        }
+    }
+}
