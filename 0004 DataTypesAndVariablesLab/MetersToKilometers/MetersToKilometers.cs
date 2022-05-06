844a8db17fb9c3ec5ad0c48f861955e3d1b23829
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-06 16:58:16 +0300

    add

---
+++ 0004 DataTypesAndVariablesLab/MetersToKilometers/MetersToKilometers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class MetersToKilometers
+{
+    static void Main()
+    {
+        int meters = int.Parse(Console.ReadLine());
+
+        decimal kilometers = meters / 1000.0M;
+
+        Console.WriteLine($"{kilometers:F2}");
+    }
+}
