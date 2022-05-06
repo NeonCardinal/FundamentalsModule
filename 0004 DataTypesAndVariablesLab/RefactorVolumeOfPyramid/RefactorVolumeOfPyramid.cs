d4955055c39b5aead99659ece73d47e4d748be55
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-06 16:59:05 +0300

    add

---
+++ 0004 DataTypesAndVariablesLab/RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class RefactorVolumeOfPyramid
+{
+    static void Main()
+    {
+        Console.Write("Length: ");
+        double lengt = double.Parse(Console.ReadLine());
+        Console.Write("Width: ");
+        double width = double.Parse(Console.ReadLine());
+        Console.Write("Height: ");
+        double height = double.Parse(Console.ReadLine());
+        double volume = (lengt * width * height) / 3;
+        Console.WriteLine($"Pyramid Volume: {volume:f2}");
+    }
+}
