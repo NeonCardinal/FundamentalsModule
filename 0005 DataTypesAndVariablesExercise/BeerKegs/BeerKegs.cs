ba4dcee5b65d669396054c3a3e3cc4ebf7d16522
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:38:03 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/BeerKegs/BeerKegs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class BeerKegs
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double greater = double.MinValue;
+        string greaterModel = "";
+
+        for(int i = 0; i < n; i++)
+        {
+            string model = Console.ReadLine();
+            double radius = double.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double volume = Math.PI * radius * radius * height;
+
+            if(volume > greater)
+            {
+                greater = volume;
+                greaterModel = model;
+            }
+        }
+
+        Console.WriteLine(greaterModel);
+    }
+}
