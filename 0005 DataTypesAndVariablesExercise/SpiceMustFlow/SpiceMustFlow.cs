0d64ea093ee4255b9a3c1563cfed60a0d18f2568
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:39:43 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/SpiceMustFlow/SpiceMustFlow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class SpiceMustFlow
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int leaving = 0;
+        int daysCounter = 0;
+        int workersConsumation = 0;
+
+        while(n >= 100)
+        {
+            daysCounter++;
+            leaving += n;
+            workersConsumation += 26;
+            n -= 10;
+            if(n < 100)
+            {
+                workersConsumation += 26;
+            }
+        }
+
+
+
+        int totalLeavings = leaving - workersConsumation;
+
+        Console.WriteLine(daysCounter);
+        Console.WriteLine(totalLeavings);
+    }
+}
