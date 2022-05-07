e0738e01e07c0fc7d90ae4a2deed1edd16184d95
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:39:11 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/PrintPartOfASCIITable/PrintPartOfASCIITable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PrintPartOfASCIITable
+{
+    static void Main()
+    {
+        int startIndex = int.Parse(Console.ReadLine());
+        int endIndex = int.Parse(Console.ReadLine());
+
+        for(int i = startIndex; i <= endIndex; i++)
+        {
+            Console.Write($"{(char)i} ");
+        }
+    }
+}
