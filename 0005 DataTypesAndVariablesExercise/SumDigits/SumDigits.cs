0ec9070e5d686fb6a3d4c0fd60710b68b6972f04
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:39:58 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/SumDigits/SumDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class SumDigits
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        while(number > 0)
+        {
+            int temp = number % 10;
+            sum += temp;
+            number /= 10;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
