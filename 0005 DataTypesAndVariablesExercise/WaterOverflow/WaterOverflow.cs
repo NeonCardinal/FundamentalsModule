b68c4e0cc7aa8536cde44b60404f69da3785da4b
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:40:47 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/WaterOverflow/WaterOverflow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class WaterOverflow
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int litters = int.Parse(Console.ReadLine());
+
+            if(litters > 255)
+            {
+                Console.WriteLine("Insufficient capacity");
+            }
+            else if(litters <= (255 - sum))
+            {
+                sum += litters;
+            }
+            else if(litters > (255 - sum))
+            {
+                Console.WriteLine("Insufficient capacity");
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
