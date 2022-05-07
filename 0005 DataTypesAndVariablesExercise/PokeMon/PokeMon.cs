80a4bebbd2e24fe5208eeddfb1f33eb5544d424a
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-07 22:38:54 +0300

    add

---
+++ 0005 DataTypesAndVariablesExercise/PokeMon/PokeMon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class PokeMon
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int counter = 0;
+        double temp = n;
+
+        while(n >= m)
+        {
+            counter++;
+            n -= m;
+
+            if(temp / 2 == n)
+            {
+                if(y != 0)
+                {
+                    n /= y;
+                }
+            }
+        }
+
+        Console.WriteLine(n);
+        Console.WriteLine(counter);
+    }
+}
