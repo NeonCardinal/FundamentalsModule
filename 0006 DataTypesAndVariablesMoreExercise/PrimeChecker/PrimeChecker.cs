73824daeff028cdf12ddcd92f73a19ecb3f12e53
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 22:28:01 +0300

    add

---
+++ 0006 DataTypesAndVariablesMoreExercise/PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class PrimeChecker
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= number; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine($"{i} -> true");
+            }
+            else
+            {
+                Console.WriteLine($"{i} -> false");
+            }
+
+        }
+    }
+}
