cd9116ee37d39f5ff65538d85d0d148bd4867c00
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 22:27:05 +0300

    add

---
+++ 0006 DataTypesAndVariablesMoreExercise/DecryptingMessage/DecryptingMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class DecryptingMessage
+{
+    static void Main()
+    {
+        int key = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        string message = "";
+
+        for (int i = 0; i < n; i++)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            char result = (char)(symbol + key);
+            message += result;
+        }
+
+        Console.WriteLine(message);
+    }
+}
