a7f972f35030ae21eea6dc595d725563d3a23522
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 23:11:41 +0300

    add

---
+++ 0007 ArraysLab/EqualArrays/EqualArrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class EqualArrays
+{
+    static void Main()
+    {
+        bool notEqual = false;
+        int sum = 0;
+        int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            if (firstArray[i] != secondArray[i])
+            {
+                Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                notEqual = true;
+                break;
+            }
+            sum += firstArray[i];
+        }
+
+        if (!notEqual)
+        {
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
