e1512a83e3384eaab92f54be562e412766bdd403
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 22:27:40 +0300

    add

---
+++ 0006 DataTypesAndVariablesMoreExercise/FromLeftToTheRight/FromLeftToTheRight.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class FromLeftToTheRight
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+            long[] array = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            if(array[0] > array[1])
+            {
+                Console.WriteLine(Math.Abs(CalculateSum(array[0])));
+            }
+            else
+            {
+                Console.WriteLine(Math.Abs(CalculateSum(array[1])));
+            }
+        }
+    }
+
+    static long CalculateSum(long number)
+    {
+        long sum = 0;
+
+        while(number != 0)
+        {
+            long temp = number % 10;
+            sum += temp;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
