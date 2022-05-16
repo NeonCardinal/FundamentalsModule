99fbc5e61ba091bf4a12acbcce6877632d99259d
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 22:27:23 +0300

    add

---
+++ 0006 DataTypesAndVariablesMoreExercise/FloatingEquality/FloatingEquality.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class FloatingEquality
+{
+    static void Main()
+    {
+        const double Eps = 0.000001;
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double result = Math.Abs(a - b);
+
+        if (result > Eps)
+        {
+            Console.WriteLine("False");
+        }
+        else if (result < Eps)
+        {
+            Console.WriteLine("True");
+        }
+        else if (result == Eps)
+        {
+            Console.WriteLine("False");
+        }
+    }
+}
