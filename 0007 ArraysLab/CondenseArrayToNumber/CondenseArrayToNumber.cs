488f2bb4ab4c88f0e10b369045bade4b14742d69
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 23:11:05 +0300

    add

---
+++ 0007 ArraysLab/CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public class CondenseArrayToNumber
+{
+    static void Main()
+    {
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int len = numbers.Length;
+
+        for (int i = 0; i < len - 1; i++)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+            for (int j = 0; j < numbers.Length - 1; j++)
+            {
+                condensed[j] = numbers[j] + numbers[j + 1];
+            }
+            numbers = condensed;
+        }
+
+        Console.WriteLine(numbers[0]);
+    }
+}
