5179fef1732613fecffde9ed4328f224863ed7db
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-16 23:12:45 +0300

    add

---
+++ 0007 ArraysLab/RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class RoundingNumbers
+{
+    static void Main()
+    {
+        double[] array = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        int[] rounded = new int[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            rounded[i] = (int)Math.Round(array[i], MidpointRounding.AwayFromZero);
+        }
+
+        for (int i = 0; i < rounded.Length; i++)
+        {
+            Console.WriteLine($"{array[i]} => {rounded[i]}");
+        }
+    }
+}
