9d2402980948f4a83bdd84a308b3e479497221b7
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 20:58:12 +0300

    add

---
+++ 0008 ArraysExercise/005.TopIntegers/TopIntegers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class TopIntegers
+{
+    static void Main()
+    {
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string greater = "";
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int currEl = array[i];
+            bool isTopInteger = true;
+
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[j] >= currEl)
+                {
+                    isTopInteger = false;
+                    break;
+                }
+            }
+
+            if (isTopInteger)
+            {
+                greater += currEl + " ";
+            }
+        }
+
+        Console.WriteLine(greater);
+    }
+}
