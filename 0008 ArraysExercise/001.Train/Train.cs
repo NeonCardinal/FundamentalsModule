18fe4813cb27e404c9b07d6eeafdc5285208c423
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 20:56:49 +0300

    add

---
+++ 0008 ArraysExercise/001.Train/Train.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Train
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+
+        int sum = 0;
+
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+            sum += array[i];
+        }
+
+        Console.WriteLine(string.Join(" ", array));
+        Console.WriteLine(sum);
+    }
+}
