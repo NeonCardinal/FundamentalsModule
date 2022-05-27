40bcc2e7c99a0b7404a180373cb5888c97073847
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 20:57:33 +0300

    add

---
+++ 0008 ArraysExercise/003.ZigZagArrays/ZigZagArrays.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+public class ZigZagArrays
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] firstArray = new int[n];
+        int[] secondArray = new int[n];
+
+        for(int i = 1; i <= n; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if(i % 2 != 0)
+            {
+                firstArray[i - 1] = input[0];
+                secondArray[i - 1] = input[1];
+            }
+            else
+            {
+                firstArray[i - 1] = input[1];
+                secondArray[i - 1] = input[0];
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", firstArray));
+        Console.WriteLine(string.Join(" ", secondArray));
+    }
+}
