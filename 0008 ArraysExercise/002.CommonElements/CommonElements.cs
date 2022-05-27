2326376e9b97507552c503e9061e1692e1f1e609
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 20:57:13 +0300

    add

---
+++ 0008 ArraysExercise/002.CommonElements/CommonElements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class CommonElements
+{
+    static void Main()
+    {
+        string[] firstArray = Console.ReadLine().Split();
+        string[] secondArray = Console.ReadLine().Split();
+        string result = string.Empty;
+
+        for(int i = 0; i < firstArray.Length; i++)
+        {
+            for(int j = 0; j < secondArray.Length; j++)
+            {
+                if (secondArray[j] == firstArray[i])
+                {
+                    result += secondArray[j] + " ";
+                }
+            }
+        }
+    }
+}
