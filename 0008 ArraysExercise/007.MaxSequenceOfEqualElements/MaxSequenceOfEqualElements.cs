39115299291003d817905247561308f6cb036f82
NeonCardinal <nvnnaidenov@gmail.com>
2022-05-27 20:58:53 +0300

    add

---
+++ 0008 ArraysExercise/007.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+public class MaxSequenceOfEqualElements
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int start = 0;
+        int len = 1;
+        int bestStart = 0;
+        int bestLen = 1;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] != arr[i + 1])
+            {
+                start = i + 1;
+                len = 1;
+            }
+            else
+            {
+                len++;
+            }
+
+            if (len > bestLen)
+            {
+                bestLen = len;
+                bestStart = start;
+            }
+        }
+
+        for (int i = bestStart; i < bestStart + bestLen; i++)
+        {
+            Console.Write($"{arr[i]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
